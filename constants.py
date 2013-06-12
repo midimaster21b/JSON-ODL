95cadbc0863d98e3126db344ed559d3290dbd42f
Joshua Edgcombe <joshedgcombe@gmail.com>
2013-06-12 15:18:48 -0400

    Add sql identifiers to constants

--- constants.py
+++ constants.py
@@ -1,2 +1,8 @@
 method_generator_prefix = "generate_"
 additional_method_modules = ['test_additional_methods']
+object_storage_descriptors = {
+    'unsigned_integer': {'sql': 'BIGINT UNSIGNED'},
+    'integer': {'sql': 'BIGINT'},
+    'short_text': {'sql': 'VARCHAR'},
+    'long_text': {'sql': 'TEXT'},
+    }
